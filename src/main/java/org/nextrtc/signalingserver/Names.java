46577a3e4f44c708c9d4f63b9177487610803c68
robikshrestha <robikshrestha@gmail.com>
2017-04-06 16:18:05 +0545

    Increase ping interval and scheduler size

--- src/main/java/org/nextrtc/signalingserver/Names.java
+++ src/main/java/org/nextrtc/signalingserver/Names.java
@@ -4,8 +4,8 @@ public interface Names {
     String EVENT_BUS = "nextRTCEventBus";
     String EVENT_DISPATCHER = "nextRTCEventDispatcher";
 
-    String SCHEDULER_SIZE = "${nextrtc.scheduler_size:10}";
+    String SCHEDULER_SIZE = "${nextrtc.scheduler_size:50}";
     String SCHEDULER_NAME = "nextRTCPingScheduler";
-    String SCHEDULED_PERIOD = "${nextrtc.ping_period:3}";
+    String SCHEDULED_PERIOD = "${nextrtc.ping_period:20}";
     String MAX_CONNECTION_SETUP_TIME = "${nextrtc.max_connection_setup_time:30}";
 }
